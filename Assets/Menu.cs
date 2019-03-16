aca89c31617559c317637e42fa110091fc863563
Sean Turner <sean@hutchgames.com>
2019-03-16 14:08:19 -0300

    Arcade menu and icon

---
+++ Assets/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+	public void LoadMenu() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+	}
+	public void LoadInvaders() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Invaders");
+	}
+	public void LoadBreakout() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Breakout");
+	}
+	public void LoadPacman() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Pacman");
+	}
+	public void LoadDefender() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Defender");
+	}
+}
