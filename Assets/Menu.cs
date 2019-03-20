ff6e01ef4561bca3e27273fa0a89179bd0a9e6f7
Sean Turner <sean@hutchgames.com>
2019-03-19 21:27:16 -0300

    Menu object persists and handles click in top/left to return to menu

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+	private void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+	private void Update() {
+		if( Input.GetMouseButtonDown(0) && (Input.mousePosition.x < 64f) && (Input.mousePosition.y > (Screen.height-64f)) )
+			LoadMenu();
+	}
 	public void LoadMenu() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+		Destroy(this.gameObject);
 	}
 	public void LoadInvaders() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Invaders");
+		gameObject.GetComponent<Canvas>().enabled = false;
 	}
 	public void LoadBreakout() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Breakout");
+		gameObject.GetComponent<Canvas>().enabled = false;
 	}
 	public void LoadPacman() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Pacman");
+		gameObject.GetComponent<Canvas>().enabled = false;
 	}
 	public void LoadDefender() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Defender");
+		gameObject.GetComponent<Canvas>().enabled = false;
 	}
 }
