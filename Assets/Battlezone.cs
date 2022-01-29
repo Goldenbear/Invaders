9b90388d9ada70d15285d56c87359cc45d833a11
Sean Turner <sean@hutchgames.com>
2022-01-29 11:36:33 -0400

    Renamed to lower case Z in Battlezone. Rough UI layout.

--- Assets/Battlezone.cs
+++ Assets/Battlezone.cs
@@ -2,12 +2,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
-public class BattleZone : MonoBehaviour {
+public class Battlezone : MonoBehaviour {
 	public class GameData : MonoBehaviour {
 		public GameObject target;
 	}
 	static int score = 0, lives = 2, level = 1;
-	GameObject explosion, player;
 	GameObject[] uiObjects = new GameObject[10];
 	List<GameObject> allObjects = new List<GameObject>(), playerLives = new List<GameObject>();
 	float[,] playerGeom = new float[,] { {-0.5f, -0.5f, 0f}, { -0.3f, -0.3f, 0f}, { 0.5f, -0.3f, 0f}, { 0.5f, -0.2f, 0f}, { -0.3f, 0.2f, 0f}, { -0.4f, 0.5f, 0f}, { -0.5f, 0.5f, 0f}, { -0.5f, -0.5f, 0f} };
@@ -21,25 +20,11 @@ public class BattleZone : MonoBehaviour {
 	Vector2 Joystick { get { for(int t=0; t<Input.touchCount; t++) {if(TouchJoy(t).magnitude<2f) return TouchJoy(t);} return KeyJoy; } }
 	bool Fire { get { for(int t=0; t<Input.touchCount; t++) {if(TouchJoy(t).x<-2f) return true; } return Input.GetKey(KeyCode.LeftShift); } }
 	void Start() {
+		gameObject.GetComponent<Camera>().orthographic = false;				// 3D perspective camera!
 		gameObject.GetComponent<Camera>().backgroundColor = Color.black;
-		gameObject.GetComponent<Camera>().orthographic = false;
-		explosion = new GameObject("Explosion");
-		explosion.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-		explosion.AddComponent<ParticleSystem>().Pause();
-		explosion.GetComponent<ParticleSystemRenderer>().material = new Material(Shader.Find("Sprites/Default"));
-		ParticleSystem.MainModule main = explosion.GetComponent<ParticleSystem>().main;
-		main.startSize = 0.025f;
-		main.startLifetime = 2f;
-		main.startSpeed = 10f;
-		main.scalingMode = ParticleSystemScalingMode.Shape;
- 		Gradient gradient = new Gradient();
-		gradient.colorKeys = new GradientColorKey[] { new GradientColorKey(Color.red, 0f), new GradientColorKey(Color.white, 0.5f), new GradientColorKey(Color.grey, 1f) };
-		allObjects.Add( player = CreateVectorObject("Player", playerGeom, 0f, 0f, 0f, 0.6f, 0.2f, 1f, 1, Color.white, true, true, 0.1f, gradient) );
-        //allObjects.Add( CreateVectorObject("Terrain1", terrainGeom, -10f,     0f, 0f, 1f, 1f, 1f, 0, Color.green) );
-        //allObjects.Add( CreateVectorObject("Terrain2", terrainGeom, -10f+40f, 0f, 0f, 1f, 1f, 1f, 0, Color.green) );
-        CreateVectorObject("Terrain1", terrainGeom, -10f,     0f, 0f, 1f, 1f, 1f, 0, Color.green).transform.parent = transform;
+ 		CreateVectorObject("Terrain1", terrainGeom, -10f, 0f, 0f, 1f, 1f, 1f, 0, Color.green).transform.parent = transform;
 		for (int i=0; i<20; i++) {
-			playerLives.Add( CreateVectorObject("Life", playerGeom, -7f+i*0.5f, 4f, 0f, 0.3f, 0.1f, 1f, 0, Color.white, i<=lives, true, 0.1f, gradient) );
+			playerLives.Add( CreateVectorObject("Life", playerGeom, 0.2f+i*0.5f, 5.2f, 0f, 0.3f, 0.1f, 1f, 0, Color.green, i<=lives, true, 0.1f) );
 			playerLives[i].transform.parent = gameObject.transform;
 		}
 		for (int i=0; i<100; i++)
@@ -50,10 +35,12 @@ public class BattleZone : MonoBehaviour {
 			uiObjects[1+i] = new GameObject("UIText");
 			uiObjects[1+i].transform.parent = uiObjects[0].transform;
 			uiObjects[1+i].AddComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
-			uiObjects[1+i].GetComponent<Text>().fontSize = Screen.width/30;
-			uiObjects[1+i].GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
-			uiObjects[1+i].GetComponent<RectTransform>().localPosition = i == 0 ? new Vector3(-Screen.width/2.4f, Screen.height/2.2f, 0f) : i == 1 ? new Vector3(0f, Screen.height/2.2f, 0f) : new Vector3(0f, 0f, 0f);
+			uiObjects[1+i].GetComponent<Text>().fontSize = i == 1 ? Screen.width/50 : Screen.width/30;
+			uiObjects[1+i].GetComponent<Text>().alignment = TextAnchor.MiddleLeft;		// Left align text within RectTransform
+			uiObjects[1+i].GetComponent<RectTransform>().pivot = new Vector2(0f, 0.5f);	// Position left of RectTransform
+			uiObjects[1+i].GetComponent<RectTransform>().localPosition = i == 0 ? new Vector3(0f, Screen.height/2.5f, 0f) : i == 1 ? new Vector3(0f, Screen.height/2.8f, 0f) : new Vector3(0f, 0f, 0f);
 			uiObjects[1+i].GetComponent<RectTransform>().sizeDelta = new Vector2(800f, 800f);
+			uiObjects[1+i].GetComponent<Text>().color = Color.green;
 		}
     }
 	GameObject CreateVectorObject(string label, float[,] geom, float pX, float pY, float pZ, float sX, float sY, float sZ, int layer, Color color, bool active = true, bool visible = true, float thickness=0.04f, Gradient gradient=null) {
@@ -79,7 +66,6 @@ public class BattleZone : MonoBehaviour {
 		return go;
 	}
 	void KillPlayer() {
-		Explode(player, 50);
 		playerLives[lives].SetActive(false);
 		gameState = (--lives >= 0) ? 1 : 3;         // Player lost a life or player dead = game over?
 		gameStateTimer = Time.unscaledTime + 3f;
@@ -88,34 +74,25 @@ public class BattleZone : MonoBehaviour {
 		lives = (score / 10000) < ((score + add) / 10000) ? ((lives < (playerLives.Count - 1)) ? lives + 1 : lives) : lives;
 		playerLives[lives].SetActive(true);
 		score += add;
-		PlayerPrefs.SetInt("DefenderHighScore", score > PlayerPrefs.GetInt("DefenderHighScore") ? score : PlayerPrefs.GetInt("DefenderHighScore"));
-	}
-	void Explode(GameObject go, int numParticles=20) {
-		go.SetActive(false);
-		explosion.transform.position = go.transform.position;
-		explosion.GetComponent<Renderer>().material.color = go.GetComponent<Renderer>().material.color;
-		explosion.GetComponent<ParticleSystem>().Emit(numParticles);
+		PlayerPrefs.SetInt("BattlezoneHighScore", score > PlayerPrefs.GetInt("BattlezoneHighScore") ? score : PlayerPrefs.GetInt("BattlezoneHighScore"));
 	}
 	void Update() {
-		uiObjects[1].GetComponent<Text>().text = string.Format("{0:00000}", score);
-		uiObjects[2].GetComponent<Text>().text = string.Format("{0:00000}", PlayerPrefs.GetInt("BattleZoneHighScore"));
+		uiObjects[1].GetComponent<Text>().text = string.Format("SCORE     {0:00000}", score);
+		uiObjects[2].GetComponent<Text>().text = string.Format("HIGH SCORE       {0:00000}", PlayerPrefs.GetInt("BattlezoneHighScore"));
 		uiObjects[3].GetComponent<Text>().text = gameState == 2 ? "ATTACK WAVE " + level + "\n COMPLETED\n\nBONUS " + allObjects.Where(x => x.layer == 3).Count() + " X 100" : gameState == 3 ? "GAME OVER" : "";
 		if (gameState > 0) {
 			if ((gameState == 1) && (Time.unscaledTime > gameStateTimer)) {
-				player.transform.position = Vector3.zero;
-				player.SetActive(true);
 				gameState = 0;
 			}
 			if ((gameState >= 2) && ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)))) {
 				level++;
 				if (gameState == 3) { score = 0; lives = 2; level = 1; }                    // New game
-				UnityEngine.SceneManagement.SceneManager.LoadScene("BattleZone");			// Reload scene for new level
+				UnityEngine.SceneManagement.SceneManager.LoadScene("Battlezone");			// Reload scene for new level
 			}
 			return;
 		}
 		List<GameObject> destroyed = new List<GameObject>(), added = new List<GameObject>();
 		if (Fire && (Time.unscaledTime > gameStateTimer)) {
-			//allObjects.Add(CreateVectorObject("Laser", bulletGeom, player.transform.position.x + Mathf.Sign(player.transform.localScale.x) * 0.6f, player.transform.position.y - 0.05f, player.transform.position.z, Mathf.Sign(player.transform.localScale.x), 0.1f, 1f, 2, Color.HSVToRGB(Random.value, 1f, 1f)));
 			gameStateTimer = Time.unscaledTime + 0.2f;
 		}
 		transform.Rotate(new Vector3(0f, Joystick.x*10f*Time.deltaTime, 0f));
