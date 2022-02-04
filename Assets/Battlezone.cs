09e8c9977382c788ebbeed2ffc14a9093f2e8e5c
Sean Turner <sean@hutchgames.com>
2022-02-04 17:46:17 -0400

    Enemy "radar" HUD text

--- Assets/Battlezone.cs
+++ Assets/Battlezone.cs
@@ -19,7 +19,7 @@ public class Battlezone : MonoBehaviour {
 	int[] tankGInd = new int[] {0, 1, 2, 3, 0, 4, 5, 1, 5, 6, 2, 6, 7, 3, 7, 4, 8, 9, 5, 9, 10, 6, 10, 11, 7, 11, 8, 12, 9, 10, 13, 11, 13, 12, 14, 15, 16, 17, 14, 18, 19, 15, 19, 20, 16, 20, 21, 17, 21, 18};
 	float[,] radarGeom = new float[,] { {-2f, -0.5f, 1f}, { -1f, -1f, 0f}, { -1f, 1f, 0f}, { -2f, 0.5f, 1f}, { -2f, -0.5f, 1f}, {-1f, -1f, 0f}, { 1f, -1f, 0f}, { 1f, 1f, 0f}, { -1f, 1f, 0f}, { -1f, -1f, 0f}, {1f, -1f, 0f}, { 2f, -0.5f, 1f}, { 2f, 0.5f, 1f}, { 1f, 1f, 0f}, { 1f, -1f, 0f}, {0f, -1f, 0f}, {0f, -1.5f, 0f} };
 	float gameStateTimer = 0f;	// Both timer for losing life and auto-fire!
-	int gameState = 0;
+	int gameState = 0, enemyRadar = 0;
 	Vector2 TouchJoy(int t) { return (Input.GetTouch(t).position - new Vector2(Screen.width-(Screen.height/4f), Screen.height/4f)) / (Screen.height/4f); }
 	Vector2 KeyJoy { get { return (Input.GetKey(KeyCode.LeftArrow)?-Vector2.right:Vector2.zero)+(Input.GetKey(KeyCode.RightArrow)?Vector2.right:Vector2.zero)+(Input.GetKey(KeyCode.DownArrow)?-Vector2.up:Vector2.zero)+(Input.GetKey(KeyCode.UpArrow)?Vector2.up:Vector2.zero); } }
 	Vector2 Joystick { get { for(int t=0; t<Input.touchCount; t++) {if(TouchJoy(t).magnitude<2f) return TouchJoy(t);} return KeyJoy; } }
@@ -28,6 +28,7 @@ public class Battlezone : MonoBehaviour {
 		gameObject.GetComponent<Camera>().orthographic = false;				// 3D perspective camera!
 		gameObject.GetComponent<Camera>().backgroundColor = Color.black;
 		gameObject.AddComponent<BoxCollider>().isTrigger = true;			// Player collider
+		gameObject.GetComponent<BoxCollider>().size = new Vector3(0.5f, 0.5f, 0.5f);
 		gameObject.layer = 1;
 		transform.Translate(new Vector3(0f, 0.3f, 0f));
  		CreateVectorObject("Terrain1", VertexArray(terrainGeom),    0f, 0f, 40f, (2f*Mathf.PI*40f)/40f, 5f, 1f, 0f, 0f, 0f, 0, Color.green, true, true, 0.1f).transform.parent = transform;
@@ -40,14 +41,14 @@ public class Battlezone : MonoBehaviour {
         	allObjects.Add( CreateVectorObject("Obstacle", VertexArray(cubeGeom), Random.Range(-40f, 40f), 0.25f, Random.Range(-40f, 40f), 0.5f, 0.25f, 0.5f, 0f, 0f, 0f, 4, Color.green) );
 		uiObjects[0] = new GameObject("UICanvas");
 		uiObjects[0].AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
-		for(int i=0; i<4; i++) {
+		for(int i=0; i<5; i++) {
 			uiObjects[1+i] = new GameObject("UIText");
 			uiObjects[1+i].transform.parent = uiObjects[0].transform;
 			uiObjects[1+i].AddComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
 			uiObjects[1+i].GetComponent<Text>().fontSize = i == 1 ? Screen.width/50 : Screen.width/30;
 			uiObjects[1+i].GetComponent<Text>().alignment = TextAnchor.MiddleLeft;		// Left align text within RectTransform
 			uiObjects[1+i].GetComponent<RectTransform>().pivot = new Vector2(0f, 0.5f);	// Position left of RectTransform
-			uiObjects[1+i].GetComponent<RectTransform>().localPosition = i == 0 ? new Vector3(0f, Screen.height/2.5f, 0f) : i == 1 ? new Vector3(0f, Screen.height/2.8f, 0f) : new Vector3(0f, 0f, 0f);
+			uiObjects[1+i].GetComponent<RectTransform>().localPosition = i == 0 ? new Vector3(0f, Screen.height/2.5f, 0f) : i == 1 ? new Vector3(0f, Screen.height/2.8f, 0f) : i == 2 ? new Vector3(-Screen.width/2.2f, Screen.height/2.5f, 0f) : i == 3 ? new Vector3(-Screen.width/2.2f, Screen.height/2.8f, 0f) : new Vector3(0f, 0f, 0f);
 			uiObjects[1+i].GetComponent<RectTransform>().sizeDelta = new Vector2(800f, 800f);
 			uiObjects[1+i].GetComponent<Text>().color = Color.green;
 		}
@@ -77,7 +78,6 @@ public class Battlezone : MonoBehaviour {
 		go.GetComponent<Renderer>().material = new Material(Shader.Find("Sprites/Default"));
 		go.GetComponent<Renderer>().material.color = color;
 		go.GetComponent<Renderer>().enabled = visible;
-		//go.AddComponent<GameData>().state = 0;
 		go.AddComponent<GameData>().stateTimeout = Time.unscaledTime + 3f;			// Required by bullets
 		go.GetComponent<GameData>().targetPos = go.transform.position;				// Required by tank AI so immediately retargets
 		return go;
@@ -96,7 +96,9 @@ public class Battlezone : MonoBehaviour {
 	void Update() {
 		uiObjects[1].GetComponent<Text>().text = string.Format("SCORE     {0:00000}", score);
 		uiObjects[2].GetComponent<Text>().text = string.Format("HIGH SCORE       {0:00000}", PlayerPrefs.GetInt("BattlezoneHighScore"));
-		uiObjects[3].GetComponent<Text>().text = gameState == 2 ? "GAME OVER" : "";
+		uiObjects[3].GetComponent<Text>().text = ((enemyRadar&1)!=0) ? "ENEMY IN RANGE" : "";
+		uiObjects[4].GetComponent<Text>().text = ((enemyRadar&2)!=0) ? "ENEMY TO LEFT" : ((enemyRadar&4)!=0) ? "ENEMY TO RIGHT" : "";
+		uiObjects[5].GetComponent<Text>().text = gameState == 2 ? "GAME OVER" : "";
 		if (gameState > 0) {
 			if ((gameState == 1) && (Time.unscaledTime > gameStateTimer)) {
 				gameState = 0;																// Back to playing
@@ -111,8 +113,9 @@ public class Battlezone : MonoBehaviour {
 		if (Fire && (Time.unscaledTime > gameStateTimer)) {
 			Vector3 pos = transform.position + transform.forward*1f;
 			allObjects.Add(CreateVectorObject("Bullet", VertexArray(bulletGeom), pos.x, pos.y, pos.z, 0.05f, 0.05f, 0.2f, 0f, transform.eulerAngles.y, 0f, 2, Color.green));
-			gameStateTimer = Time.unscaledTime + 3f;
+			gameStateTimer = Time.unscaledTime + 1f;
 		}
+		enemyRadar = 0;
 		foreach(GameObject go in allObjects) {
 			go.GetComponent<LineRenderer>().widthMultiplier = 0.005f + (Mathf.Clamp((go.transform.position-transform.position).magnitude, 1f, 10f)/10f) * 0.03f;	// Wider line width as get farther away
 			switch(go.layer) {
@@ -138,6 +141,8 @@ public class Battlezone : MonoBehaviour {
 /* Tank */		case 3: go.transform.GetChild(0).Rotate(new Vector3(0f, 60f*Time.deltaTime, 0f));		// Rotate radar
 						Vector3 desiredDir = new Vector3(transform.position.x-go.transform.position.x, 0f, transform.position.z-go.transform.position.z);
 						float driveSpeed = 1f;
+						enemyRadar |= (desiredDir.magnitude < 10f) ? 1 : 0;
+						enemyRadar |= ((Camera.main.WorldToViewportPoint(go.transform.position).x<0)||(Camera.main.WorldToViewportPoint(go.transform.position).x>1)) ? ((Vector3.Dot(desiredDir, transform.right) > 0f) ? 2 : 4) : 0;
 						if(go.GetComponent<GameData>().state == 0) {			// Drive to the target position
 							desiredDir = new Vector3(go.GetComponent<GameData>().targetPos.x-go.transform.position.x, 0f, go.GetComponent<GameData>().targetPos.z-go.transform.position.z);
 							if(desiredDir.magnitude < 0.1f) {
